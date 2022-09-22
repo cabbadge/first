ecba292482952b34c8cfdf9f5830075edff3e6a0
Sher <sher.kg15@gmail.com>
2022-09-22 16:59:51 +0600

    first commit

---
+++ js2.js
@@ -0,0 +1,20 @@
+
+"use strict";
+
+// let number = 5;
+//  const leftBorserWith =1;
+//  number=10;
+//  console.log(number);
+
+
+// const answers=[];
+// answers[0]=prompt("Как тебя зовут?");
+// answers[1]=prompt("Получить уведомление?");
+// answers[2]=prompt("Сколько тебе лет?");
+
+// document.write(answers);
+
+// const category=('toys');
+// console.log('привет '+ category)
+
+
