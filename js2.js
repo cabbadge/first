813ddf901daccd2b904e87f2b3cb14c9e2411b33
Sher <sher.kg15@gmail.com>
2022-09-27 21:35:44 +0600

    практика

--- js2.js
+++ js2.js
@@ -1,5 +1,5 @@
 
-"use strict";
+'use strict';
 
 // let number = 5;
 //  const leftBorserWith =1;
@@ -78,45 +78,6 @@
 
 
 
-//  const namberFilms=+prompt("Сколько фильмов вы уже посмотрели?", '');
-
-//     const personal = {
-//         count: namberFilms,
-//         movies:{},
-//         actors:{},
-//         genres:[],
-//         privat: false
-//     };
-
-    
-//     console.log(personal);
-
-//     if (personal.count<10){
-//         console.log("Нуб");
-//     } else if(personal =>10 && personal.count <30){
-//         console.log("Нормально нормально");
-//     }
-//     else if(personal.count > 30){
-//         console.log("Ты киноман");
-//     } else{
-//         console.log("Произошла ошибка");
-//     }
-
-//     for(let i=0; i<2;i++){
-//         const   a= prompt('Один из последних посмотренных фильмов?', ''),
-//                 b= prompt('На сколько оцените его?', '');
-
-//             if(a != null && b !=null && a !=''  && b !='' && a.length<50){
-//                 personal.movies[a]=b;
-//                 console.log('Да');
-//             } else{
-//                 console.log('Error');
-//                 i--;
-//             }
-
-                
-//     }
-
 
 // let num=20;
 // function showFirstMessage(text){
@@ -129,8 +90,92 @@
 
 
 
-function spans(a,b){
-    return(a+b);
+// function spans(a,b){
+//     return(a+b);
 
-}
- console.log(spans(13,79));
+// }
+//  console.log(spans(13,79));
+
+
+// const str="Test sher ping";
+// console.log(str.toLocaleLowerCase());
+// console.log(str.indexOf("sh"));
+// console.log(str.toLocaleUpperCase());
+// console.log(str.slice(5, 10));
+// console.log(str.substring(5,10));
+// console.log(str.substr(5,7));
+
+
+
+
+
+
+
+
+
+
+
+
+ let numberFilms;
+
+ function start(){
+    numberFilms=+prompt("Сколько фильмов вы уже посмотрели?", '');
+
+    while(numberFilms ==''|| numberFilms==null|| isNaN(numberFilms) ){
+        numberFilms=+prompt("Сколько фильмов вы уже посмотрели?", '');
+    }
+ }
+ start();
+
+    const personal = {
+        count: numberFilms,
+        movies:{},
+        actors:{},
+        genres:[],
+        privat: false
+    };
+
+    
+
+    function personalLevel(){
+        console.log(personal);
+
+        if (personal.count<10){
+            console.log("Нуб");
+        } else if(personal =>10 && personal.count <30){
+            console.log("Нормально нормально");
+        }
+        else if(personal.count > 30){
+            console.log("Ты киноман");
+        } else{
+            console.log("Произошла ошибка");
+        }
+    }
+    // personalLevel();
+
+    function remeberfimlms(){
+        for(let i=0; i<2;i++){
+            const   a= prompt('Один из последних посмотренных фильмов?', ''),
+                    b= prompt('На сколько оцените его?', '');
+    
+                if(a != null && b !=null && a !=''  && b !='' && a.length<50){
+                    personal.movies[a]=b;
+                    console.log('Да');
+                } else{
+                    console.log('Error');
+                    i--;
+                }
+        }
+    }
+    // remeberfimlms();
+
+    function showMyDB(hidden){
+        if(!hidden){
+            console.log(personal);
+        }
+    }
+    showMyDB(personal.privat);
+
+    function writeYourGenres(){
+
+    }
